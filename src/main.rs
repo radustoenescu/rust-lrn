8a85ebd3440014d0761d6ca128ceca9df63c7b5a
Radu <radu.stoe@gmail.com>
2021-12-05 22:00:30 +0200

    gcd of seq

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,14 @@ fn gcd(a: u64, b: u64) -> u64 {
 	} 	
 }
 
+fn gcd_seq( seq: &[u64] ) -> u64 {
+    let mut d = seq[0];
+    for i in &seq[1..] {
+        d = gcd( d, *i );
+    }
+    d
+}
+
 use std::str::FromStr;
 use std::env;
 
@@ -19,14 +27,9 @@ fn main() {
     for arg in env::args().skip( 1 ) {
         numbers.push( u64::from_str( &arg ).expect("arg not a number"));
     }
-    
     assert!( numbers.len() > 0, "no args passed" );
     
-    let mut d = numbers[0];
-    for n in &numbers {
-        d = gcd( d, *n);
-    }
-
+    let d = gcd_seq( &numbers );
     println!( "Numbers: {:?} gcd: {}", numbers, d );
 }
 
